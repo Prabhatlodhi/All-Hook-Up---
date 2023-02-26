d8cc6e8774996b1fb7a7b0bdedcc968387c4c928
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-02-26 22:20:37 +0530

    UseEffects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UseStatev1function from "./Hooks/UseState/UseStatev1function"; 
 import UseStatev1class from "./Hooks/UseState/UseStatev1class";
+import UseEffects from "./Hooks/UseState/UseRef/UseEffects/UseEffects";
 
 function App() {
   return (
     <div className="App">
      <UseStatev1function/>
      <UseStatev1class/>
+     <UseEffects/>
     </div>
   );
 }
---
+++ src/Hooks/UseState/UseRef/UseEffects/UseEffects.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+function UseEffects() {
+
+    const [first, setfirst] = useState(0)
+    const [second, setsecond] = useState(0)
+
+    useEffect(()=>{
+        console.log('First Value')
+    })
+
+    useEffect(()=>{
+        console.log('First Value')
+    })
+
+    const handleEffects = ()=>{
+        setfirst(first+1)
+    }
+
+    const handleEffectsPartTwo = ()=>{
+        setsecond(second+1)
+    }
+
+  return (
+    <div className='UseEffects'>
+      <h1>Use Effects in action</h1>
+      <h5>{first}</h5>
+      <button onClick={handleEffects}>Click here for Useeffect</button>
+      <h1>Use Effects in action PART TWO</h1>
+      <h5>{second}</h5>
+      <button onClick={handleEffectsPartTwo}>Click here for Useeffect- PART TWO </button>
+    </div>
+  )
+}
+
+export default UseEffects
