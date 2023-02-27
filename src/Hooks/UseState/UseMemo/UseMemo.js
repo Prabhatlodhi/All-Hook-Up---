e46b49ca5930df3a8e5b9dce8a46a5ee39271566
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-02-27 10:56:36 +0530

    UseMemo

--- src/Hooks/UseState/UseMemo/UseMemo.js
+++ src/Hooks/UseState/UseMemo/UseMemo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 function UseMemo() {
   const [count, setCount] = useState(0);
@@ -10,9 +10,27 @@ function UseMemo() {
   const handleitem = () => {
     setitem(item+1)
   };
+
+  //Below we have use "Usememo" to stop the unneccassry trigging of the function
+const StopUnneccasryTrigger = useMemo(
+    function unneccaryTrigingFunction(){
+            console.log('StopUnneccasryTrigger');
+            return count*2
+          },[count]
+)
+
+
+  //the following function is triggering in all update no matter weather it is a count or item
+//   function unneccaryTrigingFunction(){
+//     console.log('unneccaryTrigingFunction');
+//     return count*2
+//   }
+
+
   return (
     <div className="UseMemo">
       <h1>Use Memo Demonstration</h1>
+      <h1>{StopUnneccasryTrigger}</h1>
       <h3>{count}</h3>
       <button onClick={handleCount}>For Count</button>
       <h3>{item}</h3>
